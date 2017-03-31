9efb39b2528f699bcad2a6519aa4659e46ce8fbb
Frank Shimizu <frank.shimizu@est.fujitsu.com>
2017-03-31 15:02:02 +0200

    adjusted command for database update scripts

--- oscm-app/start.sh
+++ oscm-app/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-java -jar /opt/oscm-devruntime.jar org.oscm.setup.DatabaseUpgradeHandler \
+java -cp /opt/oscm-devruntime.jar org.oscm.setup.DatabaseUpgradeHandler \
      /opt/glassfish3/glassfish/domains/app-domain/config/db.properties \
 	 /opt/sqlscripts/
 	 
