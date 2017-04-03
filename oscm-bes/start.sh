0baa30106d412c59ba7e0ad226a7b73c3c28466a
Frank Shimizu <frank.shimizu@est.fujitsu.com>
2017-04-03 14:28:42 +0200

    Revert "master-indexer: Adding restart and sleep for debugging"
    This reverts commit 6392f6ff452cb2a9099b09af345c9753d94578ee.

--- oscm-bes/start.sh
+++ oscm-bes/start.sh
@@ -5,10 +5,5 @@ java -cp "/opt/oscm-devruntime.jar:/opt/lib/*" org.oscm.setup.DatabaseUpgradeHan
 	 /opt/sqlscripts/
 	 
 /opt/glassfish3/glassfish/bin/asadmin start-domain master-indexer-domain
-sleep 120
-/opt/glassfish3/glassfish/bin/asadmin stop-domain master-indexer-domain
-sleep 30
-/opt/glassfish3/glassfish/bin/asadmin start-domain master-indexer-domain
-sleep 120
 /opt/glassfish3/glassfish/bin/asadmin start-domain --verbose bes-domain
 
