a1fd5a918d536424984f6e1821dd851e09dcccee
Frank Shimizu <frank.shimizu@est.fujitsu.com>
2017-04-11 17:01:21 +0200

    birt: included mountable server.xml

--- oscm-tomcat-birt/start.sh
+++ oscm-tomcat-birt/start.sh
@@ -3,6 +3,10 @@ if [ ! -z ${BASE_URL} ]; then
     sed -i "s|^#base_url=http://127.0.0.1:8080|base_url=${BASE_URL}|g" /usr/local/tomcat/webapps/birt/WEB-INF/viewer.properties
 fi
 
+if [ -f /config/server.xml ]; then
+    cp /config/server.xml /usr/local/tomcat/conf
+fi
+
 cp /certs/*.crt /usr/local/share/ca-certificates
 /usr/sbin/update-ca-certificates
 
