bbcfec9914f227cc31f0c01687019ab872db6bdf
Frank Shimizu <frank.shimizu@est.fujitsu.com>
2017-04-11 13:54:08 +0200

    birt: added possibility to add ssl certificates to java truststore

--- oscm-tomcat-birt/start.sh
+++ oscm-tomcat-birt/start.sh
@@ -3,5 +3,7 @@ if [ ! -z ${BASE_URL} ]; then
     sed -i "s|^#base_url=http://127.0.0.1:8080|base_url=${BASE_URL}|g" /usr/local/tomcat/webapps/birt/WEB-INF/viewer.properties
 fi
 
-/usr/local/tomcat/bin/catalina.sh run
+cp /certs/*.crt /usr/local/share/ca-certificates
+/usr/sbin/update-ca-certificates
 
+/usr/local/tomcat/bin/catalina.sh run
