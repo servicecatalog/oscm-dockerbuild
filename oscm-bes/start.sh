540cf5d6487941fe523627bb1695b0fb77a2d215
Frank Shimizu <frank.shimizu@est.fujitsu.com>
2017-04-12 09:54:22 +0200

    bes, app: Added check for database availability

--- oscm-bes/start.sh
+++ oscm-bes/start.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+if [ -f /opt/glassfish3/glassfish/domains/bes-domain/config/db.properties ]; then
+    export DB_HOST=$(sed -n -e 's|^db.host=\(.*\)$|\1|gp' /opt/glassfish3/glassfish/domains/bes-domain/config/db.properties)
+    export DB_USER=$(sed -n -e 's|^db.user=\(.*\)$|\1|gp' /opt/glassfish3/glassfish/domains/bes-domain/config/db.properties)
+    until psql -h $DB_HOST -l -U $DB_USER -q >/dev/null 2>&1; do echo "Database not ready - waiting..."; sleep 3s; done
+fi
 
 java -cp "/opt/oscm-devruntime.jar:/opt/lib/*" org.oscm.setup.DatabaseUpgradeHandler \
      /opt/glassfish3/glassfish/domains/bes-domain/config/db.properties \
 	 /opt/sqlscripts/
-	 
+
 /opt/glassfish3/glassfish/bin/asadmin start-domain master-indexer-domain
 /opt/glassfish3/glassfish/bin/asadmin start-domain --verbose bes-domain
-
