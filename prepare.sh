eedb1d46c8196ee8b52595d648eac1af5feb1d32
Frank Shimizu <frank.shimizu@est.fujitsu.com>
2017-04-13 18:36:45 +0200

    first try

--- prepare.sh
+++ prepare.sh
@@ -4,6 +4,17 @@ REPO_DOCKER="`dirname \"$0\"`"
 REPO_OSCM="$1"
 BUILD_DIR="$REPO_OSCM/oscm-build/result/package"
 
+# copy resources for initdb
+mkdir -p $REPO_DOCKER/oscm-initdb/sqlscripts/bes
+cp $REPO_OSCM/oscm-devruntime/javares/sql/*.sql $REPO_DOCKER/oscm-initdb/sqlscripts/bes/
+mkdir -p $REPO_DOCKER/oscm-initdb/sqlscripts/app
+cp $REPO_OSCM/oscm-app/resources/sql/*.sql $REPO_DOCKER/oscm-initdb/sqlscripts/app/
+cp $BUILD_DIR/oscm-devruntime/oscm-devruntime.jar $REPO_DOCKER/oscm-initdb/
+cp $BUILD_DIR/oscm-common/oscm-common.jar $REPO_DOCKER/oscm-initdb/
+cp $REPO_OSCM/oscm-build/lib/log4j-1.2.16.jar $REPO_DOCKER/oscm-initdb/
+cp $REPO_OSCM/oscm-build/lib/postgresql-9.1-903.jdbc4.jar $REPO_DOCKER/oscm-initdb/
+cp $BUILD_DIR/oscm-extsvc-internal/oscm-extsvc-internal.jar $REPO_DOCKER/oscm-initdb/
+
 # copy resources for bes
 cp $BUILD_DIR/oscm-search/oscm-search.ear $REPO_DOCKER/oscm-bes/
 cp $BUILD_DIR/oscm-ear/oscm.ear $REPO_DOCKER/oscm-bes/
@@ -19,7 +30,7 @@ cp $REPO_OSCM/oscm-build/lib/log4j-1.2.16.jar $REPO_DOCKER/oscm-bes/
 cp $REPO_OSCM/oscm-build/lib/postgresql-9.1-903.jdbc4.jar $REPO_DOCKER/oscm-bes/
 cp $BUILD_DIR/oscm-extsvc-internal/oscm-extsvc-internal.jar $REPO_DOCKER/oscm-bes/
 
-mkdir $REPO_DOCKER/oscm-bes/sqlscripts/
+mkdir $REPO_DOCKER/oscm-bes/sqlscripts
 cp $REPO_OSCM/oscm-devruntime/javares/sql/*.sql $REPO_DOCKER/oscm-bes/sqlscripts/
 
 # copy resources for app
@@ -32,6 +43,5 @@ cp $REPO_OSCM/oscm-build/lib/log4j-1.2.16.jar $REPO_DOCKER/oscm-app/
 cp $REPO_OSCM/oscm-build/lib/postgresql-9.1-903.jdbc4.jar $REPO_DOCKER/oscm-app/
 cp $BUILD_DIR/oscm-extsvc-internal/oscm-extsvc-internal.jar $REPO_DOCKER/oscm-app/
 
-mkdir $REPO_DOCKER/oscm-app/sqlscripts/
+mkdir $REPO_DOCKER/oscm-app/sqlscripts
 cp $REPO_OSCM/oscm-app/resources/sql/*.sql $REPO_DOCKER/oscm-app/sqlscripts/
-
