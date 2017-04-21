0ab4cf803162d6a6704101be4a84ac2b74419a1a
Frank Shimizu <frank.shimizu@est.fujitsu.com>
2017-04-21 19:29:54 +0200

    bes, app and controller dbs working

--- oscm-app/start.sh
+++ oscm-app/start.sh
@@ -2,6 +2,7 @@
 if [ -f /opt/glassfish3/glassfish/domains/app-domain/config/db.properties ]; then
     export DB_HOST=$(sed -n -e 's|^db.host=\(.*\)$|\1|gp' /opt/glassfish3/glassfish/domains/app-domain/config/db.properties)
     export DB_USER=$(sed -n -e 's|^db.user=\(.*\)$|\1|gp' /opt/glassfish3/glassfish/domains/app-domain/config/db.properties)
+    export PGPASSWORD=$(sed -n -e 's|^db.pwd=\(.*\)$|\1|gp' /opt/glassfish3/glassfish/domains/app-domain/config/db.properties)
     export PGCONNECT_TIMEOUT=2
     until psql -h $DB_HOST -l -U $DB_USER -q >/dev/null 2>&1; do echo "Database not ready - waiting..."; sleep 3s; done
 fi
