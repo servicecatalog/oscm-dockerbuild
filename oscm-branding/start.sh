13cb609681e9aa971bfde872e18ea1bad18ff575
Frank Shimizu <frank.shimizu@est.fujitsu.com>
2017-05-17 12:38:02 +0200

    branding: load branding files from storage bucket

--- oscm-branding/start.sh
+++ oscm-branding/start.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-for file in ${BRANDING_DIR}/*.tar.gz
+/usr/bin/gcloud auth activate-service-account --key-file ${SERVICE_ACCOUNT_KEY_FILE}
+mkdir /tmp/work
+/usr/bin/gsutil cp gs://${GS_BUCKET}/*.tar.gz /tmp/work
+for file in /tmp/work/*.tar.gz
 do
     tar -zxf $file -C /usr/share/nginx/html
 done
-
 chown -R root:root /usr/share/nginx/html
+rm -r /tmp/work
 
 nginx -g 'daemon off;'
