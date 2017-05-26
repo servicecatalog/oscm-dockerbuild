c6c66462a774326f89244ad3e525df9cf45d8963
Frank Shimizu <frank.shimizu@est.fujitsu.com>
2017-05-26 17:00:11 +0200

    branding: enhance so that archive file source can be bucket or local directory

--- oscm-branding/start.sh
+++ oscm-branding/start.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
+# Create working directory
+/usr/bin/mkdir /tmp/work
 
-/usr/bin/gcloud auth activate-service-account --key-file ${GS_SERVICE_ACCOUNT_KEY_FILE}
-mkdir /tmp/work
-/usr/bin/gsutil cp gs://${GS_BUCKET}/*.tar.gz /tmp/work
+# Get branding archives from local directory
+if [ ${SOURCE} == "LOCAL" ]; then
+    /usr/bin/cp BRANDING_DIR/*.tar.gz /tmp/work
+fi
+
+# Get branding archives from bucket
+if [ ${SOURCE} == "BUCKET" ]; then
+    /usr/bin/gcloud auth activate-service-account --key-file ${GS_SERVICE_ACCOUNT_KEY_FILE}
+    /usr/bin/gsutil cp gs://${GS_BUCKET}/*.tar.gz /tmp/work
+fi
+
 for file in /tmp/work/*.tar.gz
 do
-    tar -zxf $file -C /usr/share/nginx/html
+    /usr/bin/tar -zxf $file -C /usr/share/nginx/html
 done
-chown -R root:root /usr/share/nginx/html
-rm -r /tmp/work
+/usr/bin/chown -R root:root /usr/share/nginx/html
+/usr/bin/rm -r /tmp/work
 
-nginx -g 'daemon off;'
+/usr/sbin/nginx -g 'daemon off;'
