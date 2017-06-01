5b2e8580ab7f59205e299ce080fa646782004557
Frank Shimizu <frank.shimizu@est.fujitsu.com>
2017-06-01 16:44:50 +0200

    bes,app: add database name to db wait loop

--- oscm-app/start.sh
+++ oscm-app/start.sh
@@ -53,7 +53,7 @@ echo $KEY_SECRET | sha256sum | cut -f1 -d\ | xxd -r -p | head -c 16 > $DOMAINS/a
 /usr/bin/chmod 600 /root/.pgpass
 echo "${DB_HOST_APP}:${DB_PORT_APP}:bssapp:${DB_USER_APP}:${DB_PWD_APP}" > /root/.pgpass
 export PGPASSFILE=/root/.pgpass
-until /usr/bin/psql -h ${DB_HOST_APP} -p ${DB_PORT_APP} -U ${DB_USER_APP} -l >/dev/null 2>&1; do echo "Database not ready - waiting..."; sleep 3s; done
+until /usr/bin/psql -h ${DB_HOST_APP} -p ${DB_PORT_APP} -U ${DB_USER_APP} -l ${DB_NAME_APP} >/dev/null 2>&1; do echo "Database not ready - waiting..."; sleep 3s; done
 /usr/bin/rm -f /root/.pgpass
 
 # Start domain
--- oscm-bes/start.sh
+++ oscm-bes/start.sh
@@ -63,7 +63,7 @@ echo $KEY_SECRET | sha256sum | cut -f1 -d\ | xxd -r -p | head -c 16 > $DOMAINS/b
 /usr/bin/chmod 600 /root/.pgpass
 echo "${DB_HOST_BES}:${DB_PORT_BES}:bss:${DB_USER_BES}:${DB_PWD_BES}" > /root/.pgpass
 export PGPASSFILE=/root/.pgpass
-until /usr/bin/psql -h ${DB_HOST_BES} -p ${DB_PORT_BES} -U ${DB_USER_BES} -l >/dev/null 2>&1; do echo "Database not ready - waiting..."; sleep 3s; done
+until /usr/bin/psql -h ${DB_HOST_BES} -p ${DB_PORT_BES} -U ${DB_USER_BES} -l ${DB_NAME_BES} >/dev/null 2>&1; do echo "Database not ready - waiting..."; sleep 3s; done
 /usr/bin/rm -f /root/.pgpass
 
 # Start domains
