21341db8d20e3ba72f6c104fdf9d1c53df3415e4
Arkadiusz Kowalczyk <arkadiusz.kowalczyk@ts.fujitsu.com>
2017-07-26 13:26:02 +0200

    Changed the name of tmp oscm sles based image

--- prepare_base_image.sh
+++ prepare_base_image.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-docker build -t oscm-base --build-arg HTTP_PROXY=$http_proxy --build-arg HTTPS_PROXY=$https_proxy --build-arg ACTIVATION_CODE=$ACTIVATION_CODE --build-arg EMAIL_ADDRESS=$EMAIL_ADDRESS oscm-dockerbuild/oscm-base/
+docker build -t oscm-sles-based --build-arg HTTP_PROXY=$http_proxy --build-arg HTTPS_PROXY=$https_proxy --build-arg ACTIVATION_CODE=$ACTIVATION_CODE --build-arg EMAIL_ADDRESS=$EMAIL_ADDRESS oscm-dockerbuild/oscm-sles-based/
