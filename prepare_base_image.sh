f1083c53ada323a609d500fbb852a2d7427570a0
Arkadiusz Kowalczyk <arkadiusz.kowalczyk@ts.fujitsu.com>
2017-07-26 09:25:08 +0200

    Another changes for preparing base image

--- prepare_base_image.sh
+++ prepare_base_image.sh
@@ -3,9 +3,10 @@ set -e
 
 echo $http_proxy
 echo $http_proxy
+echo $ACTIVATION_CODE
+echo $EMAIL_ADDRESS
 
 pwd
-
 ls
 
-docker build -t oscm-base --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy --build-arg ACTIVATION_CODE=$ACTIVATION_CODE --build-arg EMAIL_ADDRESS=$EMAIL_ADDRESS oscm-dockerbuild/oscm-base/
+docker build -t oscm-base --build-arg ACTIVATION_CODE=$ACTIVATION_CODE --build-arg EMAIL_ADDRESS=$EMAIL_ADDRESS oscm-dockerbuild/oscm-base/
