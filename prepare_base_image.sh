009793fb3d5d7e8ff79d38829dac047757d9ceb5
Arkadiusz Kowalczyk <arkadiusz.kowalczyk@ts.fujitsu.com>
2017-07-26 11:08:57 +0200

    Added java installation to the base image

--- prepare_base_image.sh
+++ prepare_base_image.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 set -e
 
-echo $http_proxy
-echo $http_proxy
-echo $ACTIVATION_CODE
-echo $EMAIL_ADDRESS
-
-pwd
-ls
-
 docker build -t oscm-base --build-arg HTTP_PROXY=$http_proxy --build-arg HTTPS_PROXY=$https_proxy --build-arg ACTIVATION_CODE=$ACTIVATION_CODE --build-arg EMAIL_ADDRESS=$EMAIL_ADDRESS oscm-dockerbuild/oscm-base/
