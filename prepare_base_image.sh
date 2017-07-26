95907e8e02737e357fe5b6794392ade7944045d2
Arkadiusz Kowalczyk <arkadiusz.kowalczyk@ts.fujitsu.com>
2017-07-26 10:04:44 +0200

    Changes for testing proxy settings

--- prepare_base_image.sh
+++ prepare_base_image.sh
@@ -9,4 +9,4 @@ echo $EMAIL_ADDRESS
 pwd
 ls
 
-docker build -t oscm-base --build-arg ACTIVATION_CODE=$ACTIVATION_CODE --build-arg EMAIL_ADDRESS=$EMAIL_ADDRESS oscm-dockerbuild/oscm-base/
+docker build -t oscm-base --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy --build-arg ACTIVATION_CODE=$ACTIVATION_CODE --build-arg EMAIL_ADDRESS=$EMAIL_ADDRESS oscm-dockerbuild/oscm-base/
