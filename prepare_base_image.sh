69eb918810a5a9987a1f369e4e4823cf82ed2554
Arkadiusz Kowalczyk <arkadiusz.kowalczyk@ts.fujitsu.com>
2017-07-26 08:49:15 +0200

    Changed base image path

--- prepare_base_image.sh
+++ prepare_base_image.sh
@@ -4,4 +4,8 @@ set -e
 echo $http_proxy
 echo $http_proxy
 
-docker build -t oscm-base --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy --build-arg ACTIVATION_CODE=$ACTIVATION_CODE --build-arg EMAIL_ADDRESS=$EMAIL_ADDRESS oscm-base/
+pwd
+
+ls
+
+docker build -t oscm-base --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy --build-arg ACTIVATION_CODE=$ACTIVATION_CODE --build-arg EMAIL_ADDRESS=$EMAIL_ADDRESS oscm-dockerbuild/oscm-base/
