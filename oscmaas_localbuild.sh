a0b16154492970eadfc6143451e08d81552780c9
Frank Shimizu <frank.shimizu@est.fujitsu.com>
2017-07-27 17:18:21 +0200

    added sles based db image to localbuild script

--- oscmaas_localbuild.sh
+++ oscmaas_localbuild.sh
@@ -129,6 +129,13 @@ else
 	docker build -t oscm-app:${GIT_SOURCE} oscm-dockerbuild/oscm-app
 fi
 
+# Build database image
+if [ ${PROXY_ENABLED} -eq 1 ]; then
+	docker build -t oscm-db:${GIT_SOURCE} --build-arg HTTP_PROXY="http://${HTTP_PROXY_HOST}:${HTTP_PROXY_PORT}" --build-arg HTTPS_PROXY="http://${HTTPS_PROXY_HOST}:${HTTPS_PROXY_PORT}" oscm-dockerbuild/oscm-db
+else
+	docker build -t oscm-db:${GIT_SOURCE} --build-arg oscm-dockerbuild/oscm-db
+fi
+
 # Build reverse proxy
 if [ ${PROXY_ENABLED} -eq 1 ]; then
 	docker build -t oscm-proxy:${GIT_SOURCE} --build-arg http_proxy="http://${HTTP_PROXY_HOST}:${HTTP_PROXY_PORT}" --build-arg https_proxy="http://${HTTPS_PROXY_HOST}:${HTTPS_PROXY_PORT}" oscm-dockerbuild/oscm-proxy
