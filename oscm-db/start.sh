2f0e1822f9f2cb4d19fe622e40c577675fbeceba
Frank Shimizu <frank.shimizu@est.fujitsu.com>
2017-07-28 13:54:50 +0200

    oscm-db: added start script

---
+++ oscm-db/start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if [ ! -d /var/lib/postgresql/data ]; then
+    mkdir -p /var/lib/postgresql/data
+fi
+sed -i 's|POSTGRES_DATADIR="~postgres/data"|POSTGRES_DATADIR="/var/lib/postgresql/data"|g' /etc/sysconfig/postgresql
+chown -R postgres: /var/lib/postgresql
+chmod 700 /var/lib/postgresql/data
+su - postgres -c 'initdb -U postgres -D /var/lib/postgresql/data > /tmp/postgres-initdb.log 2>&1'
+su - postgres -c 'sed -e "s|#*max_prepared_transactions.*|max_prepared_transactions = 50|g" -e "s|#*max_connections.*|max_connections = 250|g" /usr/share/postgresql94/postgresql.conf.sample > /var/lib/postgresql/data/postgresql.conf'
+su - postgres -c 'postgres -D /var/lib/postgresql/data'
