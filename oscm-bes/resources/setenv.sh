9fd99d915cad1277069da98b725768b7a0895a2c
Michal <michal.grubski@ts.fujitsu.com>
2017-09-07 18:47:58 +0200

    Changing start scripts for bes and app

--- oscm-bes/resources/setenv.sh
+++ oscm-bes/resources/setenv.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-JAVA_OPTS="-Djava.security.auth.login.config=/opt/apache-tomee-plume-7.0.3/conf/jaas.config -Dorg.apache.el.parser.SKIP_IDENTIFIER_CHECK=true"
+JAVA_OPTS="-Djava.security.auth.login.config=/opt/bes/apache-tomee-plume-7.0.3/conf/jaas.config -Dorg.apache.el.parser.SKIP_IDENTIFIER_CHECK=true"
 export JAVA_OPTS
