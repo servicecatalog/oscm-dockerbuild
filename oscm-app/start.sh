6c1ad54d9348d0c874248e28279da976b8151ad5
Michal <michal.grubski@ts.fujitsu.com>
2017-09-08 12:01:33 +0200

    Updated database hosts

--- oscm-app/start.sh
+++ oscm-app/start.sh
@@ -3,9 +3,9 @@
 # Wait for database
 /usr/bin/touch /root/.pgpass
 /usr/bin/chmod 600 /root/.pgpass
-echo "${DB_HOST_BES}:${DB_PORT_BES}:${DB_NAME_BES}:${DB_USER_BES}:${DB_PWD_BES}" > /root/.pgpass
+echo "${DB_HOST_APP}:${DB_PORT_APP}:${DB_NAME_APP}:${DB_USER_APP}:${DB_PWD_APP}" > /root/.pgpass
 export PGPASSFILE=/root/.pgpass
-until /usr/bin/psql -h ${DB_HOST_BES} -p ${DB_PORT_BES} -U ${DB_USER_BES} -l ${DB_NAME_BES} >/dev/null 2>&1; do echo "Database not ready - waiting..."; sleep 3s; done
+until /usr/bin/psql -h ${DB_HOST_APP} -p ${DB_PORT_APP} -U ${DB_USER_APP} -l ${DB_NAME_APP} >/dev/null 2>&1; do echo "Database not ready - waiting..."; sleep 3s; done
 /usr/bin/rm -f /root/.pgpass
 
 
