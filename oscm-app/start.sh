2fd09928a7edce838f8a6bf3a923cb90996aaa61
Michal <michal.grubski@ts.fujitsu.com>
2017-09-12 14:50:27 +0200

    Changed starting script

--- oscm-app/start.sh
+++ oscm-app/start.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-read
+# Wait for database
+/usr/bin/touch /root/.pgpass
+/usr/bin/chmod 600 /root/.pgpass
+echo "${DB_HOST_APP}:${DB_PORT_APP}:${DB_NAME_APP}:${DB_USER_APP}:${DB_PWD_APP}" > /root/.pgpass
+export PGPASSFILE=/root/.pgpass
+until /usr/bin/psql -h ${DB_HOST_APP} -p ${DB_PORT_APP} -U ${DB_USER_APP} -l ${DB_NAME_APP} >/dev/null 2>&1; do echo "Database not ready - waiting..."; sleep 3s; done
+/usr/bin/rm -f /root/.pgpass
+
+# Start domains
+/opt/apache-tomee-plume-7.0.3/bin/catalina.sh run
