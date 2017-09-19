81f728e7d85831042efec7c58cc5320a8f383d6c
Michal <michal.grubski@ts.fujitsu.com>
2017-09-19 14:56:11 +0200

    Fixing starting scripts

--- oscm-bes/start.sh
+++ oscm-bes/start.sh
@@ -8,5 +8,10 @@ export PGPASSFILE=/root/.pgpass
 until /usr/bin/psql -h ${DB_HOST_BES} -p ${DB_PORT_BES} -U ${DB_USER_BES} -l ${DB_NAME_BES} >/dev/null 2>&1; do echo "Database not ready - waiting..."; sleep 3s; done
 /usr/bin/rm -f /root/.pgpass
 
+
 # Start domains
-/opt/apache-tomee-plume-7.0.3/bin/catalina.sh run
+if [ ${DEBUG_ENABLED} ]; then
+	/opt/apache-tomee-plume-7.0.3/bin/catalina.sh jpda run
+else
+	/opt/apache-tomee-plume-7.0.3/bin/catalina.sh run
+fi
