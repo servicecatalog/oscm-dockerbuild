45ed56423264773dc9b5806c5121a2e17148b613
Dawid Chojnacki <Dawid.Chojnacki@ts.fujitsu.com>
2017-09-20 13:11:56 +0200

    Another try to remove bad character at the end of line.

--- oscm-bes/resources/setenv.sh
+++ oscm-bes/resources/setenv.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-JAVA_OPTS=" -Dorg.apache.el.parser.SKIP_IDENTIFIER_CHECK=true -Djava.security.auth.login.config=/opt/apache-tomee-plume-7.0.3/conf/jaas.config "
+JAVA_OPTS=%JAVA_OPTS% -Dorg.apache.el.parser.SKIP_IDENTIFIER_CHECK=true
+JAVA_OPTS=%JAVA_OPTS% -Djava.security.auth.login.config=/opt/apache-tomee-plume-7.0.3/conf/jaas.config
 export JAVA_OPTS
