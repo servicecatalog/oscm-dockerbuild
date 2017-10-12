7d3e2017f8b3087b3507e24d294307b41b0ecf8e
Frank Shimizu <frank.shimizu@est.fujitsu.com>
2017-10-12 12:57:43 +0200

    fixes #38 oscm-db: new variable and functionality to change superuser password

--- oscm-db/start.sh
+++ oscm-db/start.sh
@@ -11,3 +11,6 @@ rm -f /tmp/pw
 su - postgres -c 'sed -e "s|#*max_prepared_transactions.*|max_prepared_transactions = 50|g" -e "s|#*max_connections.*|max_connections = 250|g" -e "s|#*listen_addresses =.*|listen_addresses = '"'"'*'"'"'|g" /usr/share/postgresql94/postgresql.conf.sample > /var/lib/postgresql/data/postgresql.conf'
 su - postgres -c 'echo "host all all all md5" >> /var/lib/postgresql/data/pg_hba.conf'
 su - postgres -c 'postgres -D /var/lib/postgresql/data'
+if [ ${DB_SUPERPWD} != "postgres" ]; then
+    su - postgres -c "ALTER USER postgres WITH SUPERUSER PASSWORD ${DB_SUPERPWD};"
+fi
