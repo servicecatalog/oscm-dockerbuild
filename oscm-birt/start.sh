6bd7d28616189560a9dfc59fea7d41b903577311
Michał Grubski <michal.grubski@ts.fujitsu.com>
2017-10-12 11:14:33 +0200

    Issue 33 (#35)
    * Removing unneeded libraries for birt
    
    * Modified starting command
    
    * Reverted to old dockerfile
    
    * Added vim to birt container

--- oscm-birt/start.sh
+++ oscm-birt/start.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+if [ ! -z ${BASE_URL} ]; then
+    sed -i "s|^#base_url=http://127.0.0.1:8080|base_url=${BASE_URL}|g" /srv/tomcat/webapps/birt/WEB-INF/viewer.properties
+fi
 
-/opt/tomcat-9/apache-tomcat-9.0.1/bin/catalina.sh run
+cp /certs/*.crt /usr/share/pki/trust/anchors
+/usr/sbin/update-ca-certificates
+
+su - tomcat -c 'source /etc/tomcat/tomcat.conf ; export CATALINA_BASE CATALINA_HOME CATALINA_TMPDIR ; /usr/sbin/tomcat-sysd start'
