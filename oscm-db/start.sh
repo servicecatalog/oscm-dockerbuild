2d7ecfc8d8537ecb69bef89316d233256aa462d2
Frank Shimizu <frank.shimizu@est.fujitsu.com>
2017-10-16 16:41:19 +0200

    oscm-db: fix command order

--- oscm-db/start.sh
+++ oscm-db/start.sh
@@ -7,10 +7,6 @@ chown -R postgres: /var/lib/postgresql
 chmod 700 /var/lib/postgresql/data
 sed -i 's|POSTGRES_DATADIR="~postgres/data"|POSTGRES_DATADIR="/var/lib/postgresql/data"|g' /etc/sysconfig/postgresql
 
-# Alter configuration for Catalog Manager
-su - postgres -c 'sed -e "s|#*max_prepared_transactions.*|max_prepared_transactions = 50|g" -e "s|#*max_connections.*|max_connections = 250|g" -e "s|#*listen_addresses =.*|listen_addresses = '"'"'*'"'"'|g" /usr/share/postgresql94/postgresql.conf.sample > /var/lib/postgresql/data/postgresql.conf'
-su - postgres -c 'echo "host all all all md5" >> /var/lib/postgresql/data/pg_hba.conf'
-
 # Create temporary superuser password file
 echo ${DB_SUPERPWD} > /tmp/pw
 
@@ -19,6 +15,10 @@ su - postgres -c 'initdb -U postgres -D /var/lib/postgresql/data --pwfile=/tmp/p
 
 # Remove temporary superuser password file
 rm -f /tmp/pw
+
+# Alter configuration for Catalog Manager
+su - postgres -c 'sed -e "s|#*max_prepared_transactions.*|max_prepared_transactions = 50|g" -e "s|#*max_connections.*|max_connections = 250|g" -e "s|#*listen_addresses =.*|listen_addresses = '"'"'*'"'"'|g" /usr/share/postgresql94/postgresql.conf.sample > /var/lib/postgresql/data/postgresql.conf'
+su - postgres -c 'echo "host all all all md5" >> /var/lib/postgresql/data/pg_hba.conf'
 
 # Start PostgreSQL
 su - postgres -c 'postgres -D /var/lib/postgresql/data'
