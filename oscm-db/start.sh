59dc1d0acbeb2ad02e820c652bfa69d4ca2087cf
Frank Shimizu <frank.shimizu@est.fujitsu.com>
2017-10-19 11:13:42 +0200

    oscm-db: set superuser password if db exists

--- oscm-db/start.sh
+++ oscm-db/start.sh
@@ -13,6 +13,15 @@ echo ${DB_SUPERPWD} > /tmp/pw
 # Initialize database
 su - postgres -c 'initdb -U postgres -D /var/lib/postgresql/data --pwfile=/tmp/pw > /tmp/postgres-initdb.log 2>&1'
 
+# If initdb failed because a database already exists, only set the
+# superuser password again, in case it changed
+if [ $? -ne 0 ]; then
+    su - postgres -c 'postgres -D /var/lib/postgresql/data' &
+    until su - postgres -c 'psql -U postgres -l' >/dev/null 2>&1; do echo "Database not ready - waiting..."; sleep 3s; done
+    su - postgres -c "psql -U postgres -c \"ALTER USER Postgres WITH PASSWORD '${DB_SUPERPWD}';\""
+    su - postgres -c 'pg_ctl -D /var/lib/postgresql/data stop'
+fi
+
 # Remove temporary superuser password file
 rm -f /tmp/pw
 
