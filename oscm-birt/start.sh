6377b3e0e7a003a41dea4967b96b03d27c7d28bf
Frank Shimizu <frank.shimizu@est.fujitsu.com>
2017-10-24 18:39:44 +0200

    oscm-birt: fix setting the base url

--- oscm-birt/start.sh
+++ oscm-birt/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ ! -z ${BASE_URL} ]; then
-    sed -i "s|^#base_url=http://127.0.0.1:8080|base_url=${BASE_URL}|g" /srv/tomcat/webapps/birt/WEB-INF/viewer.properties
+    sed -i "s|^#base_url=http://127.0.0.1:8080|base_url=https://${HOST_FQDN}:8681|g" /srv/tomcat/webapps/birt/WEB-INF/viewer.properties
 fi
 
 # Copy SSL private key and certificate, generate Keystore and copy to Tomcat config
