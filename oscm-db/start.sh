0551340381673e6ff3588d367502d0095923d71c
Frank Shimizu <frank.shimizu@est.fujitsu.com>
2017-11-27 18:25:44 +0100

    oscm-db: fix changed path of sample file

--- oscm-db/start.sh
+++ oscm-db/start.sh
@@ -26,7 +26,7 @@ fi
 rm -f /tmp/pw
 
 # Alter configuration for Catalog Manager
-su - postgres -c 'sed -e "s|#*max_prepared_transactions.*|max_prepared_transactions = 50|g" -e "s|#*max_connections.*|max_connections = 250|g" -e "s|#*listen_addresses =.*|listen_addresses = '"'"'*'"'"'|g" /usr/share/postgresql94/postgresql.conf.sample > /var/lib/postgresql/data/postgresql.conf'
+su - postgres -c 'sed -e "s|#*max_prepared_transactions.*|max_prepared_transactions = 50|g" -e "s|#*max_connections.*|max_connections = 250|g" -e "s|#*listen_addresses =.*|listen_addresses = '"'"'*'"'"'|g" /usr/share/postgresql96/postgresql.conf.sample > /var/lib/postgresql/data/postgresql.conf'
 su - postgres -c 'echo "host all all all md5" >> /var/lib/postgresql/data/pg_hba.conf'
 
 # Start PostgreSQL
