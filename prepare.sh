27854d5c5fd4702b77564976b739cbdfaee4c381
marcin-fest <marcin.maciaszczyk@ts.fujitsu.com>
2018-08-01 14:42:18 +0200

    Align with changes in oscm repository

--- prepare.sh
+++ prepare.sh
@@ -76,7 +76,7 @@ cp $REPO_OSCM/oscm-devruntime/javares/sql/*.sql $REPO_DOCKER/oscm-initdb/sqlscri
 mkdir -p $REPO_DOCKER/oscm-initdb/sqlscripts/app
 cp $REPO_OSCM/oscm-app/resources/sql/*.sql $REPO_DOCKER/oscm-initdb/sqlscripts/app/
 mkdir -p $REPO_DOCKER/oscm-initdb/sqlscripts/vmware
-cp $REPO_OSCM/oscm-app-vmware-setup/resources/sql/*.sql $REPO_DOCKER/oscm-initdb/sqlscripts/vmware/
+cp $REPO_OSCM/oscm-app-vmware/resources/sql/*.sql $REPO_DOCKER/oscm-initdb/sqlscripts/vmware/
 
 # copy resources for core
 cp $BUILD_DIR/oscm-search/oscm-search.ear $REPO_DOCKER/oscm-core/
