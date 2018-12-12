9eae96fcaf4b6eea1baa2e819c84c6b9dd1d4d37
Lorenz Goebel <lorenzgoebel@gmx.de>
2018-12-12 13:23:36 +0100

    Adapt tomcat birt deploy.

--- oscm-birt/deploy.sh
+++ oscm-birt/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 su -s /bin/bash -c 'source /etc/tomcat/tomcat.conf ; source /etc/sysconfig/tomcat ; export CATALINA_BASE CATALINA_HOME CATALINA_TMPDIR ; /usr/sbin/tomcat start' -g tomcat tomcat &
 sleep 30 ; \
-su -s /bin/bash -c 'source /etc/tomcat/tomcat.conf ; source /etc/sysconfig/tomcat ; export CATALINA_BASE CATALINA_HOME CATALINA_TMPDIR ; /usr/sbin/tomcat start' -g tomcat tomcat &
+su -s /bin/bash -c 'source /etc/tomcat/tomcat.conf ; source /etc/sysconfig/tomcat ; export CATALINA_BASE CATALINA_HOME CATALINA_TMPDIR ; /usr/sbin/tomcat stop' -g tomcat tomcat &
