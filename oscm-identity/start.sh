3f2e095ce5484a31cc6c21d5494e1e304a683557
goebel <lorenz.goebel@est.fujitsu.com>
2019-06-24 11:03:32 +0200

    Test.

--- oscm-identity/start.sh
+++ oscm-identity/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 
-java -Dhttp.proxyHost=$PROXY_HTTP_HOST -Dhttp.proxyPort=$PROXY_HTTP_PORT -Dhttps.proxyHost=$PROXY_HTTPS_HOST -Dhttps.proxyPort=$PROXY_HTTPS_PORT -Djava.security.egd=file:/dev/./urandom -jar /opt/main.jar
+# java -Dhttp.proxyHost=$PROXY_HTTP_HOST -Dhttp.proxyPort=$PROXY_HTTP_PORT -Dhttps.proxyHost=$PROXY_HTTPS_HOST -Dhttps.proxyPort=$PROXY_HTTPS_PORT -Djava.security.egd=file:/dev/./urandom -jar /opt/main.jar
