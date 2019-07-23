2b6c355b47e4cd0ccf3848da5377376d20bf3ae6
Arkadiusz Kowalczyk <arkadiusz.kowalczyk@ts.fujitsu.com>
2019-07-23 12:07:09 +0200

    Modified debug level in oscm-identity

--- oscm-identity/start.sh
+++ oscm-identity/start.sh
@@ -5,7 +5,7 @@ HTTPS_PROXY_HOST=$(echo $https_proxy | cut -d'/' -f3 | cut -d':' -f1)
 HTTPS_PROXY_PORT=$(echo $https_proxy | cut -d'/' -f3 | cut -d':' -f2)
 
 if [ ${TOMEE_DEBUG} ]; then
-	java -Dhttp.proxyHost=$PROXY_HTTP_HOST -Dhttp.proxyPort=$PROXY_HTTP_PORT -Dhttps.proxyHost=$PROXY_HTTPS_HOST -Dhttps.proxyPort=$PROXY_HTTPS_PORT -Djava.security.egd=file:/dev/./urandom -jar /opt/identity/main.jar -Dagentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=9000
+	java -Dhttp.proxyHost=$PROXY_HTTP_HOST -Dhttp.proxyPort=$PROXY_HTTP_PORT -Dhttps.proxyHost=$PROXY_HTTPS_HOST -Dhttps.proxyPort=$PROXY_HTTPS_PORT -Djava.security.egd=file:/dev/./urandom -jar /opt/identity/main.jar -Dagentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=9000 -Dlogging.level.org.oscm.identity=DEBUG
 else
 	java -Dhttp.proxyHost=$PROXY_HTTP_HOST -Dhttp.proxyPort=$PROXY_HTTP_PORT -Dhttps.proxyHost=$PROXY_HTTPS_HOST -Dhttps.proxyPort=$PROXY_HTTPS_PORT -Djava.security.egd=file:/dev/./urandom -jar /opt/identity/main.jar
 fi
