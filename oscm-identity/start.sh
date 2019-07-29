f367c86f7cdafa37cc4d8899c6703bcfcb301567
worf <worf@estworf.intern.est.fujitsu.com>
2019-07-29 17:02:02 +0200

    replace ssl.key and ssl.crt

--- oscm-identity/start.sh
+++ oscm-identity/start.sh
@@ -6,10 +6,16 @@ HTTPS_PROXY_PORT=$(echo $https_proxy | cut -d'/' -f3 | cut -d':' -f2)
 
 java -Dhttp.proxyHost=$PROXY_HTTP_HOST -Dhttp.proxyPort=$PROXY_HTTP_PORT -Dhttps.proxyHost=$PROXY_HTTPS_HOST -Dhttps.proxyPort=$PROXY_HTTPS_PORT -Djava.security.egd=file:/dev/./urandom -jar /opt/identity/main.jar
 
+# Copy SSL private key and certificate
+find /import/ssl/privkey -type f -exec cp -f {} /opt/ssl.key \;
+find /import/ssl/cert -type f -exec cp -f {} /opt/ssl.crt \;
+find /import/ssl/chain -type f -exec cp -f {} /opt/ssl.chain \;
+
 # Import SSL certificates into truststore
-find /import/ssl/certs -type f -exec cp {} /usr/share/pki/ca-trust-source/anchors \;
+find /import/ssl/cert -type f -exec cp {} /usr/share/pki/ca-trust-source/anchors \;
 for certfile in /usr/share/pki/ca-trust-source/anchors/*; do
     trust anchor --store $certfile
 done
 find /etc/pki/ca-trust/source/anchors -type f -name "*.p11-kit" -exec sed -i 's|^certificate-category: other-entry$|certificate-category: authority|g' {} \;
 /usr/bin/update-ca-trust
+
