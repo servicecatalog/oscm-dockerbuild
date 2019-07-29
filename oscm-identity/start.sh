4cb048db5d8b3fd5daeb92aa1bb19a1e5f65072a
worf <worf@estworf.intern.est.fujitsu.com>
2019-07-29 14:07:23 +0200

    import certificates into truststore

--- oscm-identity/start.sh
+++ oscm-identity/start.sh
@@ -5,3 +5,11 @@ HTTPS_PROXY_HOST=$(echo $https_proxy | cut -d'/' -f3 | cut -d':' -f1)
 HTTPS_PROXY_PORT=$(echo $https_proxy | cut -d'/' -f3 | cut -d':' -f2)
 
 java -Dhttp.proxyHost=$PROXY_HTTP_HOST -Dhttp.proxyPort=$PROXY_HTTP_PORT -Dhttps.proxyHost=$PROXY_HTTPS_HOST -Dhttps.proxyPort=$PROXY_HTTPS_PORT -Djava.security.egd=file:/dev/./urandom -jar /opt/identity/main.jar
+
+# Import SSL certificates into truststore
+find /import/certs -type f -exec cp {} /usr/share/pki/ca-trust-source/anchors \;
+for certfile in /usr/share/pki/ca-trust-source/anchors/*; do
+    trust anchor --store $certfile
+done
+find /etc/pki/ca-trust/source/anchors -type f -name "*.p11-kit" -exec sed -i 's|^certificate-category: other-entry$|certificate-category: authority|g' {} \;
+/usr/bin/update-ca-trust
