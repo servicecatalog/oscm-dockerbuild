07b005ded8aa8e9326f0325868ea498eb782c44e
worf <worf@estworf.intern.est.fujitsu.com>
2019-07-29 15:02:19 +0200

    use correct directory

--- oscm-identity/start.sh
+++ oscm-identity/start.sh
@@ -7,7 +7,7 @@ HTTPS_PROXY_PORT=$(echo $https_proxy | cut -d'/' -f3 | cut -d':' -f2)
 java -Dhttp.proxyHost=$PROXY_HTTP_HOST -Dhttp.proxyPort=$PROXY_HTTP_PORT -Dhttps.proxyHost=$PROXY_HTTPS_HOST -Dhttps.proxyPort=$PROXY_HTTPS_PORT -Djava.security.egd=file:/dev/./urandom -jar /opt/identity/main.jar
 
 # Import SSL certificates into truststore
-find /import/certs -type f -exec cp {} /usr/share/pki/ca-trust-source/anchors \;
+find /import/ssl/certs -type f -exec cp {} /usr/share/pki/ca-trust-source/anchors \;
 for certfile in /usr/share/pki/ca-trust-source/anchors/*; do
     trust anchor --store $certfile
 done
