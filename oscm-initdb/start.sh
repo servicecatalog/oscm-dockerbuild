c3a4dfa329e63771f0c1ff1234a1903bc7a261c0
worf <worf@estworf.intern.est.fujitsu.com>
2019-08-07 12:21:21 +0200

    without function call

--- oscm-initdb/start.sh
+++ oscm-initdb/start.sh
@@ -282,17 +282,17 @@ if [ $TARGET == "SAMPLE_DATA" ]; then
 fi
 
 #Update the sampe users, if defined in the var.env template
-genSQLUpdateUser
+#genSQLUpdateUser
 ADMIN_USER_ID = ${ADMIN_USER_ID}
 CUSTOMER_USER_ID = ${CUSTOMER_USER_ID}
 SUPPLIER_USER_ID = ${SUPPLIER_USER_ID}
 if [ ! -z "$ADMIN_USER_ID"]; then
-	PGPASSWORD=${DB_SUPERPWD} psql -h $DB_HOST_CORE -p $DB_PORT_CORE -U $DB_SUPERUSER -f /opt/sqlscripts/administrator.sql $DB_NAME_CORE
+	PGPASSWORD=${DB_SUPERPWD} psql -h $DB_HOST_CORE -p $DB_PORT_CORE -U $DB_SUPERUSER -f /opt/sqlscripts/administrator.sql $DB_NAME_CORE $ADMIN_USER_ID
 fi	
 if [ ! -z "$CUSTOMER_USER_ID"]; then
-	PGPASSWORD=${DB_SUPERPWD} psql -h $DB_HOST_CORE -p $DB_PORT_CORE -U $DB_SUPERUSER -f /opt/sqlscripts/customer.sql $DB_NAME_CORE
+	PGPASSWORD=${DB_SUPERPWD} psql -h $DB_HOST_CORE -p $DB_PORT_CORE -U $DB_SUPERUSER -f /opt/sqlscripts/customer.sql $DB_NAME_CORE $CUSTOMER_USER_ID
 fi	
 if [ ! -z "$SUPPLIER_USER_ID"]; then
-	PGPASSWORD=${DB_SUPERPWD} psql -h $DB_HOST_CORE -p $DB_PORT_CORE -U $DB_SUPERUSER -f /opt/sqlscripts/supplier.sql $DB_NAME_CORE
+	PGPASSWORD=${DB_SUPERPWD} psql -h $DB_HOST_CORE -p $DB_PORT_CORE -U $DB_SUPERUSER -f /opt/sqlscripts/supplier.sql $DB_NAME_CORE $SUPPLIER_USER_ID
 fi	
 
