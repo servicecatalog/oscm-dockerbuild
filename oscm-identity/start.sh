6aee85f2b65181bd5885d32d6a68b93a7280bdf9
Arkadiusz Kowalczyk <arkadiusz.kowalczyk@ts.fujitsu.com>
2019-08-08 08:38:33 +0200

    SSL configuration of oscm-identity (#182)
    * add create certificate
    
    * add target path
    
    * add directorys
    
    * remove unused steps
    
    * add new volumes
    
    * import certificates into truststore
    
    * use correct directory
    
    * replace ssl.key and ssl.crt
    
    * add keystore
    
    * fix bug
    
    * change to keystore
    
    * change to keystore
    
    * put keystore in identity directory
    
    * use claspath
    
    * use classpath
    
    * change keystore name and add import to java truststore
    
    * remove whitespace
    
    * add new line
    
    * Updated https port

--- oscm-identity/start.sh
+++ oscm-identity/start.sh
@@ -4,8 +4,43 @@ HTTP_PROXY_PORT=$(echo $http_proxy | cut -d'/' -f3 | cut -d':' -f2)
 HTTPS_PROXY_HOST=$(echo $https_proxy | cut -d'/' -f3 | cut -d':' -f1)
 HTTPS_PROXY_PORT=$(echo $https_proxy | cut -d'/' -f3 | cut -d':' -f2)
 
+# Copy SSL private key and certificate
+find /import/ssl/privkey -type f -exec cp -f {} /opt/ssl.key \;
+find /import/ssl/cert -type f -exec cp -f {} /opt/ssl.crt \;
+find /import/ssl/chain -type f -exec cp -f {} /opt/ssl.chain \;
+
+if [ -f /opt/ssl.chain ]; then
+    openssl pkcs12 -export \
+        -in /opt/ssl.crt \
+        -inkey /opt/ssl.key \
+        -out /opt/identity/ssl.p12 \
+        -CAfile /opt/ssl.chain \
+        -chain \
+        -passout pass:changeit
+else
+    openssl pkcs12 -export \
+        -in /opt/ssl.crt \
+        -inkey /opt/ssl.key \
+        -out /opt/identity/ssl.p12 \
+        -passout pass:changeit
+        
+fi
+
+# Import SSL certificates into truststore
+find /import/certs -type f -exec cp {} /usr/share/pki/ca-trust-source/anchors \;
+for certfile in /usr/share/pki/ca-trust-source/anchors/*; do
+    trust anchor --store $certfile
+done
+find /import/ssl/cert -type f -exec cp {} /usr/share/pki/ca-trust-source/anchors \;
+for certfile in /usr/share/pki/ca-trust-source/anchors/*; do
+    trust anchor --store $certfile
+done
+find /etc/pki/ca-trust/source/anchors -type f -name "*.p11-kit" -exec sed -i 's|^certificate-category: other-entry$|certificate-category: authority|g' {} \;
+/usr/bin/update-ca-trust
+
 if [ ${TOMEE_DEBUG} ]; then
 	java -Dhttp.proxyHost=$PROXY_HTTP_HOST -Dhttp.proxyPort=$PROXY_HTTP_PORT -Dhttps.proxyHost=$PROXY_HTTPS_HOST -Dhttps.proxyPort=$PROXY_HTTPS_PORT -Djava.security.egd=file:/dev/./urandom -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=9000 -Dlogging.level.org.oscm.identity=DEBUG -jar /opt/identity/main.jar
 else
 	java -Dhttp.proxyHost=$PROXY_HTTP_HOST -Dhttp.proxyPort=$PROXY_HTTP_PORT -Dhttps.proxyHost=$PROXY_HTTPS_HOST -Dhttps.proxyPort=$PROXY_HTTPS_PORT -Djava.security.egd=file:/dev/./urandom  jar /opt/identity/main.jar
 fi
+
