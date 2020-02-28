f7bca233f6537ec23cc8473972964cba5264d410
cworf91 <christian.worf@est.fujitsu.com>
2020-02-28 16:11:17 +0100

    start container

--- oscm-deployer/start.sh
+++ oscm-deployer/start.sh
@@ -114,6 +114,7 @@ if [ ${INITDB} == "true" ]; then
     docker-compose -f docker-compose-initdb.yml -p $(basename ${DOCKER_PATH}) up oscm-initdb-controller-azure
     docker-compose -f docker-compose-initdb.yml -p $(basename ${DOCKER_PATH}) up oscm-initdb-controller-vmware
     docker-compose -f docker-compose-initdb.yml -p $(basename ${DOCKER_PATH}) up oscm-initdb-controller-shell
+    docker-compose -f docker-compose-initdb.yml -p $(basename ${DOCKER_PATH}) up oscm-initdb-controller-sample
     # If the user wants us to import sample data, do it now
     if [ ${SAMPLE_DATA} == "true" ]; then
         docker-compose -f docker-compose-initdb.yml -p $(basename ${DOCKER_PATH}) up oscm-initdb-sample
