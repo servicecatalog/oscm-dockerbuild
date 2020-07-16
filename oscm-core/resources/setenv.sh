f146327beda00d0cb0b1997e88fcc49cc078f833
cworf91 <christian.worf@est.fujitsu.com>
2020-07-16 15:40:13 +0200

    add copyright header

--- oscm-core/resources/setenv.sh
+++ oscm-core/resources/setenv.sh
@@ -1,3 +1,11 @@
+ #*****************************************************************************
+ #*                                                                           *
+ #* Copyright FUJITSU LIMITED 2020                                            *
+ #*                                                                           *
+ #* Creation Date: 16-07-2020                                                 *
+ #*                                                                           *
+ #*****************************************************************************
+
 #!/bin/sh
 JAVA_OPTS=$JAVA_OPTS" -Dorg.apache.el.parser.SKIP_IDENTIFIER_CHECK=true"
 JAVA_OPTS=$JAVA_OPTS" -Djava.security.auth.login.config=/opt/apache-tomee/conf/jaas.config"
