992e2b3470e8153cbf1f1d9829598db608104ee9
cworf91 <christian.worf@est.fujitsu.com>
2021-01-18 15:55:00 +0100

    refactor

--- oscm-initdb/start.sh
+++ oscm-initdb/start.sh
@@ -123,7 +123,6 @@ if [ $TARGET == "CORE" ]; then
 	set -a
 	# Read configuration files
 	source /target/.env
-	source /target/var.env
 	# Disable automatic exporting of variables
 	set +a
 	echo "HOST_FQDN="${HOST_FQDN}
