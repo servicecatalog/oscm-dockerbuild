2fe5ed39fd90cceac06d125818c1c4ec12fac6c3
cworf91 <christian.worf@est.fujitsu.com>
2021-01-18 11:09:06 +0100

    add debugg statement

--- oscm-initdb/start.sh
+++ oscm-initdb/start.sh
@@ -120,6 +120,9 @@ if [ $TARGET == "CORE" ]; then
 	set +a
 	echo "HOST_FQDN="${HOST_FQDN}
 	echo "IMAGE_DB="${IMAGE_DB}
+	echo "OSCM_IDENTITY_URL=${OSCM_IDENTITY_URL}
+	echo "MP_ERROR_REDIRECT_HTTPS=${MP_ERROR_REDIRECT_HTTPS}
+	echo "OSCM_CORE_URL=${OSCM_CORE_URL}
 
 	# Generate property files from environment
 	genPropertyFilesCORE
