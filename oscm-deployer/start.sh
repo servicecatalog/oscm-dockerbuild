6aebfa20cda079c62d620a884207dc6b7e099d05
cworf91 <christian.worf@est.fujitsu.com>
2021-01-25 16:02:16 +0100

    update syslog template with variables

--- oscm-deployer/start.sh
+++ oscm-deployer/start.sh
@@ -12,6 +12,16 @@
 COMPOSE_CONFIG_PATH=/opt
 TARGET_PATH=/target
 LOCKFILE=${TARGET_PATH}/oscm-deployer.lock
+LOGGING=
+LOCAL=
+LOCAL1=
+LOCAL2=
+LOCAL3=
+LOCAL4=
+LOCAL5=
+LOCAL6=
+LOCAL7=
+
 
 # If ${TARGET_PATH}/var.env does not exist, just copy the template for the operator and exit
 if [ ! -f ${TARGET_PATH}/var.env ] || [ ! -f ${TARGET_PATH}/.env ]; then
@@ -118,6 +128,19 @@ envsubst '$DOCKER_PATH $IMAGE_DB $IMAGE_INITDB $LOG_LEVEL' \
 < ${COMPOSE_CONFIG_PATH}/docker-compose-initdb.yml.template \
 > ${TARGET_PATH}/docker-compose-initdb.yml
 if [ ${SYSLOG} == "true" ]; then
+    LOGGING="logging:\n
+      driver: syslog\n
+      options:\n
+        syslog-facility:" 
+    LOCAL=local
+    LOCAL1=local1
+    LOCAL2=local2
+    LOCAL3=local3
+    LOCAL4=local4
+    LOCAL5=local5
+    LOCAL6=local6
+    LOCAL7=local7
+        
     envsubst < ${COMPOSE_CONFIG_PATH}/docker-compose-oscm-syslog.yml.template \
     > ${TARGET_PATH}/docker-compose-oscm.yml
 else
