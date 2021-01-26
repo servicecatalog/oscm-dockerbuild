f0b17f6c981d1787174f6a4750e5005d1cf80bdc
cworf91 <christian.worf@est.fujitsu.com>
2021-01-26 11:37:30 +0100

    format

--- oscm-deployer/start.sh
+++ oscm-deployer/start.sh
@@ -119,18 +119,18 @@ envsubst '$DOCKER_PATH $IMAGE_DB $IMAGE_INITDB $LOG_LEVEL' \
 < ${COMPOSE_CONFIG_PATH}/docker-compose-initdb.yml.template \
 > ${TARGET_PATH}/docker-compose-initdb.yml
 if [ ${SYSLOG} == "true" ]; then
-    LOGGING="logging:\n
-      driver: syslog\n
-      options:\n
-        syslog-facility:" 
-    LOCAL=local
-    LOCAL1=local1
-    LOCAL2=local2
-    LOCAL3=local3
-    LOCAL4=local4
-    LOCAL5=local5
-    LOCAL6=local6
-    LOCAL7=local7
+    LOGGING="logging:
+      driver: syslog
+      options:
+        syslog-facility:\"" 
+    LOCAL=local\"
+    LOCAL1=local1\"
+    LOCAL2=local2\"
+    LOCAL3=local3\"
+    LOCAL4=local4\"
+    LOCAL5=local5\"
+    LOCAL6=local6\"
+    LOCAL7=local7\"
         
     envsubst < ${COMPOSE_CONFIG_PATH}/docker-compose-oscm-syslog.yml.template \
     > ${TARGET_PATH}/docker-compose-oscm.yml
