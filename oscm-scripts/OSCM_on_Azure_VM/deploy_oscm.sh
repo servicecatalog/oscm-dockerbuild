ee5f2084e0729273f7e4795024838c51a340734d
kabacinsp <kabacinskip@gmail.com>
2021-01-27 11:31:14 +0100

    Missing .env file

--- oscm-scripts/OSCM_on_Azure_VM/deploy_oscm.sh
+++ oscm-scripts/OSCM_on_Azure_VM/deploy_oscm.sh
@@ -27,6 +27,7 @@ install_oscm () {
   publicIP=$(curl ifconfig.me)
 
   sudo wget https://raw.githubusercontent.com/servicecatalog/oscm-dockerbuild/master/oscm-deployer/templates/var.env.template
+  sudo wget https://raw.githubusercontent.com/servicecatalog/oscm-dockerbuild/master/oscm-deployer/templates/env.template
   sudo wget https://raw.githubusercontent.com/servicecatalog/oscm-dockerbuild/master/oscm-deployer/resources/proxy.conf.template
 
   echo -e -n "${Cyan}Enter a database password: \n${White}"
@@ -35,7 +36,7 @@ install_oscm () {
   echo -e -n "${Cyan}Enter a admin password: \n${White}"
   read -s adminPwd < /dev/tty
 
-  echo -e -n "${Cyan}Enter a email suffix: \n${White}"
+  echo -e -n "${Cyan}Enter a email suffix (mydomain.onmicrosoft.com): \n${White}"
   read -s suffix < /dev/tty
 
   administrator="administrator@${suffix}"
@@ -51,9 +52,11 @@ install_oscm () {
   sed -i 's/customer/'$customer'/g' var.env.template
   sed -i 's/reseller/'$reseller'/g' var.env.template
   sed -i 's/INTERNAL/OIDC/g' var.env.template
+  sed -i 's/${HOST_FQDN}/'$publicIP'/g' env.template
   sed -i 's/${HOST_FQDN}/'$publicIP'/g' proxy.conf.template
   sed -i 's/${FQDN}/'$publicIP'/g' proxy.conf.template
   sudo cp ./var.env.template /docker/var.env
+  sudo cp ./env.template /docker/.env
   sudo docker run --name deployer2 --rm -v /docker:/target -v /var/run/docker.sock:/var/run/docker.sock -e INITDB=true -e STARTUP=true -e SAMPLE_DATA=true -e PROXY=true docker.io/servicecatalog/oscm-deployer:latest
   sudo cp ./proxy.conf.template /docker/config/oscm-proxy/data/proxy.conf
 
