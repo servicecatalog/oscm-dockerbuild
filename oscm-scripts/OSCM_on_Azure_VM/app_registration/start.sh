e2a9bbb366e1341170d7a5799668565293df5f80
KabacinskiPiotr <piotr.kabacinski@ts.fujitsu.com>
2021-01-28 14:06:49 +0100

    Download files inside the method

--- oscm-scripts/OSCM_on_Azure_VM/app_registration/start.sh
+++ oscm-scripts/OSCM_on_Azure_VM/app_registration/start.sh
@@ -3,22 +3,31 @@
 Cyan='\033[1;35m'
 White='\033[1;37m'
 
-wget https://raw.githubusercontent.com/servicecatalog/oscm-dockerbuild/Task%23321_Add_scripts_for_setup_with_OIDC/oscm-scripts//OSCM_on_Azure_VM/app_registration/steps.sh
-wget https://raw.githubusercontent.com/servicecatalog/oscm-dockerbuild/Task%23321_Add_scripts_for_setup_with_OIDC/oscm-scripts/OSCM_on_Azure_VM/app_registration/application-template.json
-wget https://raw.githubusercontent.com/servicecatalog/oscm-dockerbuild/Task%23321_Add_scripts_for_setup_with_OIDC/oscm-scripts/OSCM_on_Azure_VM/app_registration/response.json
-wget https://raw.githubusercontent.com/servicecatalog/oscm-dockerbuild/Task%23321_Add_scripts_for_setup_with_OIDC/oscm-scripts/OSCM_on_Azure_VM/app_registration/rr_operations.sh
-wget https://raw.githubusercontent.com/servicecatalog/oscm-dockerbuild/Task%23321_Add_scripts_for_setup_with_OIDC/oscm-scripts/OSCM_on_Azure_VM/app_registration/tenant-template.properties
-
-. ./steps.sh
+get_files () {
+  wget https://raw.githubusercontent.com/servicecatalog/oscm-dockerbuild/Task%23321_Add_scripts_for_setup_with_OIDC/oscm-scripts//OSCM_on_Azure_VM/app_registration/steps.sh
+  wget https://raw.githubusercontent.com/servicecatalog/oscm-dockerbuild/Task%23321_Add_scripts_for_setup_with_OIDC/oscm-scripts/OSCM_on_Azure_VM/app_registration/application-template.json
+  wget https://raw.githubusercontent.com/servicecatalog/oscm-dockerbuild/Task%23321_Add_scripts_for_setup_with_OIDC/oscm-scripts/OSCM_on_Azure_VM/app_registration/response.json
+  wget https://raw.githubusercontent.com/servicecatalog/oscm-dockerbuild/Task%23321_Add_scripts_for_setup_with_OIDC/oscm-scripts/OSCM_on_Azure_VM/app_registration/rr_operations.sh
+  wget https://raw.githubusercontent.com/servicecatalog/oscm-dockerbuild/Task%23321_Add_scripts_for_setup_with_OIDC/oscm-scripts/OSCM_on_Azure_VM/app_registration/tenant-template.properties
+}
 
 #Provide Azure AD credentials
-read -p "${Cyan}Enter a application (client) ID: ${White}" client_id
-read -p "${Cyan}Enter a client secret: ${White}" client_secret
-read -p "${Cyan}Enter a directory (tenant) ID: ${White}" tenant_name
+echo -e -n "${Cyan}Enter a application (client) ID: ${White}"
+read client_id
+echo -e -n "${Cyan}Enter a client secret: ${White}"
+read client_secret
+echo -e -n "${Cyan}Enter a directory (tenant) ID: ${White}"
+read tenant_name
 
 #Provide application properties
-read -p "${Cyan}Enter a directory (tenant) ID: ${White}" app_display_name
-read -p "${Cyan}Enter a directory (tenant) ID: ${White}" app_ip
+echo -e -n "${Cyan}Enter a directory (tenant) ID: ${White}"
+read app_display_name
+echo -e -n "${Cyan}Enter a directory (tenant) ID: ${White}"
+read app_ip
+
+get_files
+
+. ./steps.sh
 
 prepare_input
 
